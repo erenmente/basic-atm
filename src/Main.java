c60d1f1c7b17973dcc02115412e5dc9cbf8c42a9
Eren <muhammeterenmente2@gmail.com>
2025-10-29 19:56:55 +0300

    Önceki projedeki mantık hataları düzeltilip daha temiz ve anlaşılabilir şekilde yazıldı.

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,101 @@
 import java.util.Scanner;
 
-class Atm{
-    public static void main(String[] args){
-        Scanner input=new Scanner(System.in);//Scanner sınıfı klavyeden veri alır.
-        BankaHesabi benimHesabim=new BankaHesabi(1000,1500);
+public class Main {
+
+    public static void main(String[] args) {
+
+        ATM atm = new ATM();
+        Scanner input = new Scanner(System.in);
         int secim;
-        boolean dongu=true;
+        boolean dongu = true;
 
-        while(dongu){//while true olduğu sürece devam ettir.
+        while (dongu) {//while true olduğu sürece devam ettir.
             System.out.println("------------ERENBANK ATM'YE HOŞGELDİNİZ------------");
-
             System.out.println("Lütfen aşağıdan yapmak istediğiniz işlemi seçin.");
             System.out.println("1:Para çekmek");
             System.out.println("2:Para yatırmak");
             System.out.println("3:Çıkış");
-            System.out.println("Mevcut bakiyeniz:"+benimHesabim.bakiye+"TL");
+            System.out.println("Mevcut bakiyeniz:" + atm.bakiye + "TL");
             System.out.print("Yapmak istediğiniz işlemin numarasını yazınız(1-3):");
-            if(input.hasNextInt()){//girilen değerin sayısal bir değer olup olmadığına bakar.
-                secim=input.nextInt();
-            }
-            else{
+
+            if (input.hasNextInt()) {//girilen değerin sayısal bir değer olup olmadığına bakar.
+                secim = input.nextInt();
+            } else {
                 System.out.println("Lütfen geçerli bir değer girin.");
                 input.next();
                 continue;
             }
-            switch(secim){
+            switch (secim) {
                 case 1://Para çekme kısmı
-                    benimHesabim.paraCekme(input);
+                    System.out.print("Cekmek istediğiniz bakiyeyi giriniz:");
+                    if (input.hasNextInt()) {
+                        int cekilecekTutar = input.nextInt();
+                        atm.paraCekme(cekilecekTutar);
+                    } else {
+                        System.out.println("Lütfen geçerli bir değer girin.");
+                        input.next();
+                        continue;
+                    }
                     break;
 
                 case 2://Para yatırma kısmı
-                    benimHesabim.paraYatirma(input);
+                    System.out.print("Yatırmak istediğiniz bakiyeyi giriniz:");
+                    if (input.hasNextInt()) {
+                        int yatirilacakTutar = input.nextInt();
+                        atm.paraYatirma(yatirilacakTutar);
+                    } else {
+                        System.out.println("Lütfen geçerli bir değer girin.");
+                        input.next();
+                        continue;
+                    }
                     break;
                 case 3://Çıkış yapma kısmı
                     System.out.println("İyi günler dileriz.");
                     input.close();//temzilik yapar.
-                    dongu=false;//bulunduğu metoda son verir.
+                    dongu = false;//bulunduğu metoda son verir.
                     break;
                 default:
                     System.out.println("Geçersiz seçim. Lütfen 1-3 arası bir değer girin.");
 
             }
         }
+    }
+}
 
+class ATM {
+
+    int bakiye = 1000;
+    int gunlukParaCekmeLimiti;
+
+    public void paraCekme(int miktar) {
+
+        gunlukParaCekmeLimiti = 1000;
+
+        if (miktar > bakiye) {
+            System.out.println("Yetersiz bakiyeyi.");
+        } else if (miktar > gunlukParaCekmeLimiti) {
+            System.out.println("Günlük limiti aştınız. Kalan limitiniz:" + gunlukParaCekmeLimiti);
+        } else if (miktar <= 0) {
+            System.out.println("0'dan büyük değer giriniz.");
+        } else {
+            System.out.println("İşleminiz başarıyla gerçekleştirilmiştir.");
+            bakiye = bakiye - miktar;
+            gunlukParaCekmeLimiti = gunlukParaCekmeLimiti - miktar;
+            System.out.println("Güncel bakiyeniz:" + bakiye);
+            System.out.println("Güncel limitiniz:" + gunlukParaCekmeLimiti);
+        }
+
+    }
+
+    public void paraYatirma(int miktar) {
+
+        if (miktar > 0) {
+            bakiye = miktar + bakiye;
+        } else {
+            System.out.println("Lütfen pozitif bir değer giriniz.");
+        }
 
     }
 }
+
+
